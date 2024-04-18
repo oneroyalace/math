57cff4070059520f2018a97a3b6b7d044fd1ec17
oneroyalace <oneroyalace@gmail.com>
2024-04-18 07:52:12 -0400

    multi: conservative vector fields

--- probability/hw/box.py
+++ probability/hw/box.py
@@ -1,5 +1,6 @@
 import math
 import random
+from collections import Counter
 
 # box = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1]
 # # print(len(box))
@@ -97,13 +98,87 @@ box = range(10_000)
 
 # Review 14ii
 
-box = range(1,6+1)
-total_winnings = 0
-trials = 1_000_000
+# box = range(1,6+1)
+# total_winnings = 0
+# trials = 1_000_000
+# for _ in range(trials):
+#     rolls = random.choices(box, k=4)
+#     num_evens = sum([1 for roll in rolls if roll %2 == 0])
+#     all_same = int(len(set(rolls)) == 1)
+#     total_winnings += num_evens * 3
+#     total_winnings += all_same * 10
+# print(total_winnings/trials)
+
+
+# box = ["R", "Y", "G", "V"]
+
+# trials = 1_000_000
+# all_four = 0
+# for _ in range(trials):
+#     choices = random.sample(box, k=6, counts=[5,5,5,5])
+#     if len(set(choices)) == 4:
+#         all_four += 1
+# print(all_four/1_000_000)
+
+# HW 5 #4
+# n = 10_000
+# trials = 10_000
+# box = range(1,n)
+# not_selected_results = []
+
+# for _ in range(trials):
+#     choices = random.choices(box, k=n)
+#     not_selected = (n - len(set(choices)))/n
+#     not_selected_results.append(not_selected)
+# print(sum(not_selected_results)/trials)
+# print(pow((n-1)/n, n))
+ 
+#  # HW 5 # 11
+# box = ["R"] *10 + ["B"] * 4
+# trials = 1_000_000
+# successes = 0
+# for _ in range(trials):
+#     k = random.choice(range(1,6+1))
+#     choices = random.sample(box, k)
+#     if sum([1 for ball in choices if ball == "B"]) >= 4:
+#         successes += 1
+# print(successes/trials)
+
+# HW 7 # 5
+# trials = 100_000
+# y_outcomes = []
+# for _ in range(trials):
+#     X = random.uniform(2,4)
+#     Y = math.pow(X,2) + 1
+#     y_outcomes.append(Y)
+
+# for i in range(1,18):
+#     print(f"P(Y leq {i} = ", (math.sqrt(i-1)-2)/2)
+#     print(f"actual Y leq {i}: ", len([y for y in y_outcomes if y <= i])/len(y_outcomes))
+# # print(Counter(y_outcomes))
+
+
+# HW 8 #5
+
+# trials = 100_000
+# y_outcomes = []
+# for _ in range(trials):
+#     u_1 = random.uniform(0,1)
+#     u_2 = random.uniform(0,1)
+#     y_outcomes.append(max(u_1, u_2))
+
+# for c in range(11):
+#     pct_below = len([y for y in y_outcomes if y <= c*(0.1)])/len(y_outcomes)
+#     print(f"pct below  {c*0.1}: {pct_below}")
+
+
+trials = 100_000
+num, denom = 0, 0
 for _ in range(trials):
-    rolls = random.choices(box, k=4)
-    num_evens = sum([1 for roll in rolls if roll %2 == 0])
-    all_same = int(len(set(rolls)) == 1)
-    total_winnings += num_evens * 3
-    total_winnings += all_same * 10
-print(total_winnings/trials)
+    u_1 = random.uniform(0,1)
+    u_2 = random.uniform(0,1)
+    if (max(u_1, u_2) >= 1/2):
+        denom += 1
+        if (min(u_1, u_2) <= 1/4):
+            num += 1
+print(num/denom)
